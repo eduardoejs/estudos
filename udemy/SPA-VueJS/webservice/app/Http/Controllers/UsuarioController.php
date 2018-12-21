4f609e5fca68b5492b5b25da51244607c3739594
Eduardo Silva <eduardo.ejs@hotmail.com>
2018-12-21 14:05:22 -0200

    Aula 50 - Passando a logica para o controller

--- udemy/SPA-VueJS/webservice/app/Http/Controllers/UsuarioController.php
+++ udemy/SPA-VueJS/webservice/app/Http/Controllers/UsuarioController.php
@@ -30,4 +30,143 @@ class UsuarioController extends Controller
 
         return ['status' => false];
     }
+
+    public function cadastrar(Request $request){
+
+        $validacao = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validacao->fails()){
+            return $validacao->errors();
+        }
+
+        $imagem = '/perfils/default-profile.jpg';
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'imagem' => $imagem
+        ]);
+
+        //criando e retornando token do usuario
+        $user->token = $user->createToken($user->email)->accessToken;
+        $user->imagem = asset($user->imagem);
+
+        return $user;
+    }
+
+    public function perfil(Request $request){
+        $user = $request->user();
+
+    if(isset($request->password)){
+        $validacao = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => ['required','string','email','max:255', Rule::unique('users')->ignore($user->id)],
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validacao->fails()){
+            return $validacao->errors();
+        }
+
+        $user->password = bcrypt($request->password);
+    }else{
+        $validacao = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => ['required','string','email','max:255', Rule::unique('users')->ignore($user->id)],
+        ]);
+
+        if($validacao->fails()){
+            return $validacao->errors();
+        }
+    }
+
+    $user->name = $request->name;
+    $user->email = $request->email;
+
+    if(isset($request->imagem)){
+
+        //Validacao da imagem Base 64
+        Validator::extend('base64image', function ($attribute, $value, $parameters, $validator) {
+            $explode = explode(',', $value);
+            $allow = ['png', 'jpg', 'svg','jpeg'];
+            $format = str_replace(
+                [
+                    'data:image/',
+                    ';',
+                    'base64',
+                ],
+                [
+                    '', '', '',
+                ],
+                $explode[0]
+            );
+            // check file format
+            if (!in_array($format, $allow)) {
+                return false;
+            }
+            // check base64 format
+            if (!preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $explode[1])) {
+                return false;
+            }
+            return true;
+        });
+
+        $valiacao = Validator::make($request->all(), [
+            'imagem' => 'base64image',
+
+        ],['base64image'=>'Imagem inválida']);
+
+        if($valiacao->fails()){
+          return $valiacao->errors();
+        }
+        //
+
+        $time = time();
+        $diretorioPai = 'perfils';
+        $diretorioImagem = $diretorioPai.DIRECTORY_SEPARATOR.'perfil_id_'.$user->id;
+        $ext = substr($request->imagem, 11, strpos($request->imagem, ';') - 11);
+        //return [$ext];
+
+        $urlImagem = $diretorioImagem.DIRECTORY_SEPARATOR.$time.'.'.$ext;
+
+        $file = str_replace('data:image/'.$ext.';base64,', '', $request->imagem);
+        //return [$file];
+
+        //transformando o base64 em arquivo
+        $file = base64_decode($file);
+
+        if(!file_exists($diretorioPai)){
+            mkdir($diretorioPai, 0700);
+        }
+
+        if($user->imagem){
+            if(file_exists($user->imagem)){
+                unlink($user->imagem);
+            }
+        }
+
+        if(!file_exists($diretorioImagem)){
+            mkdir($diretorioImagem, 0700);
+        }
+
+        //persistindo 0 arquivo no servidor
+        file_put_contents($urlImagem, $file);
+        $user->imagem = $urlImagem;
+    }
+
+    $user->save();
+
+    $user->imagem = asset($user->imagem);//retorno a url da imagem com o dominio do servidor
+    $user->token = $user->createToken($user->email)->accessToken;
+    return $user;
+    }
+
+    public function usuario(Request $request){
+        return $request->user();
+    }
 }
